db127efeef72361daacaf04ce59c460519d81ef1
sunyoung <sunnyk5780@gmail.com>
2021-01-02 12:42:31 +0900

    URL 쿼리 사용하기

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import qs from 'qs';
 
-const About = () => {
+const About = ({ location }) => {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true, // 문자열 맨 앞의 ?를 생략하기 위함
+  });
+  const showDetail = query.detail === 'true';
+
   return (
     <div>
       <h1>About</h1>
       <p>This is an example for react router.</p>
+      {showDetail && <p>detail 값을 true로 설정했습니다.</p>}
     </div>
   );
 };
